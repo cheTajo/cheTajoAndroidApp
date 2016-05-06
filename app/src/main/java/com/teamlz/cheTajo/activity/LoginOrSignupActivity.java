9f44bd7893ab96d620df37c5e9afc87d85a39138
lollo <lorenzo.sciarra1994@gmail.com>
2016-05-06 03:54:01 +0200

    facebook authentication added, small style changes

--- app/src/main/java/com/teamlz/cheTajo/activity/LoginOrSignupActivity.java
+++ app/src/main/java/com/teamlz/cheTajo/activity/LoginOrSignupActivity.java
@@ -1,58 +1,83 @@
 package com.teamlz.cheTajo.activity;
 
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
-import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.afollestad.materialdialogs.DialogAction;
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.afollestad.materialdialogs.Theme;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginResult;
+import com.facebook.login.LoginManager;
 import com.facebook.login.widget.LoginButton;
+
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.teamlz.cheTajo.R;
-import com.teamlz.cheTajo.object.User;
-import com.teamlz.cheTajo.object.Utils;
 
-import java.util.Arrays;
-import java.util.Map;
+import com.teamlz.cheTajo.R;
 
 public class LoginOrSignUpActivity extends AppCompatActivity {
     private AppCompatEditText emailEditText;
     private AppCompatEditText passwordEditText;
-    private LoginButton facebookButton;
     private AppCompatButton manualLoginButton;
     private AppCompatButton facebookLoginButton;
+
     private Firebase myFirebase;
-    private String TAG = "LOGIN AND SIGNUP";
-    final Context thisContext = this;
+    private Firebase.AuthStateListener authStateListener;
+    private ProgressDialog authProgressDialog;
+    private AuthData facebookAuthData;
+
+    private LoginButton facebookButton;
+    private CallbackManager facebookCallbackManager;
+    private AccessTokenTracker facebookAccessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login_or_sign_up);
 
-        //initialize variables
+        //Initialize variables
         myFirebase = new Firebase(getResources().getString(R.string.firebase_url));
+        facebookLoginButton = (AppCompatButton) findViewById(R.id.facebook_sign_in_button);
+        manualLoginButton = (AppCompatButton) findViewById(R.id.manual_sign_in_button);
         emailEditText = (AppCompatEditText) findViewById(R.id.email);
         passwordEditText = (AppCompatEditText) findViewById(R.id.password);
-        manualLoginButton = (AppCompatButton) findViewById(R.id.manual_sign_in_button);
+
+        myFirebase.unauth();
+        LoginManager.getInstance().logOut();
+
+        //Set up facebook button
         facebookButton = (LoginButton) findViewById(R.id.facebook_button);
-        facebookLoginButton = (AppCompatButton) findViewById(R.id.facebook_sign_in_button);
+        facebookCallbackManager = CallbackManager.Factory.create();
+        facebookAccessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken != null) {
+                    authProgressDialog.show();
+                    myFirebase.authWithOAuthToken("facebook", currentAccessToken.getToken(), new Firebase.AuthResultHandler() {
+                        @Override
+                        public void onAuthenticated(AuthData authData) {
+                            authProgressDialog.hide();
+                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                            finish();
+                            startActivity(i);
+                        }
+
+                        @Override
+                        public void onAuthenticationError(FirebaseError firebaseError) {
+                            authProgressDialog.hide();
+                            Toast.makeText(getApplicationContext(), "Autenticazione fallita", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                else myFirebase.unauth();
+            }
+        };
 
         //Set up manual login button
         assert manualLoginButton != null;
@@ -63,9 +88,9 @@ public class LoginOrSignUpActivity extends AppCompatActivity {
                 String password = passwordEditText.getText().toString();
 
                 if (email.equals("test")) {
-                    Intent i = new Intent (getApplicationContext(), MainActivity.class);
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    finish();
                     startActivity(i);
-                    return;
                 }
 
                 else if (email.equals("") || password.equals("")) {
@@ -77,28 +102,6 @@ public class LoginOrSignUpActivity extends AppCompatActivity {
             }
         });
 
-        //Set up facebook button
-        facebookButton.setReadPermissions(Arrays.asList("email"));
-
-        // Callback registration
-        CallbackManager callbackManager = CallbackManager.Factory.create();
-        facebookButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
-            }
-
-            @Override
-            public void onCancel() {
-                // App code
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-            }
-        });
-
         //Set up facebook login button
         assert facebookLoginButton != null;
         facebookLoginButton.setOnClickListener(new View.OnClickListener() {
@@ -107,13 +110,37 @@ public class LoginOrSignUpActivity extends AppCompatActivity {
                 facebookButton.performClick();
             }
         });
+
+        authProgressDialog = new ProgressDialog(this);
+        authProgressDialog.setTitle("Attendi");
+        authProgressDialog.setMessage("Autenticazione in corso...");
+        authProgressDialog.setCancelable(false);
+        authProgressDialog.show();
+
+        authStateListener = new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                authProgressDialog.hide();
+                facebookAuthData = authData;
+            }
+        };
+        myFirebase.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        facebookAccessTokenTracker.stopTracking();
+        myFirebase.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        facebookCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void logInOrSignUp (String email, String password) {
-        final String signUpEmail = email;
-        final String signUpPassword = password;
-        final String[] credential = new String[2];
-
         myFirebase.authWithPassword(email, password, new Firebase.AuthResultHandler() {
             @Override
             public void onAuthenticated(AuthData authData) {
