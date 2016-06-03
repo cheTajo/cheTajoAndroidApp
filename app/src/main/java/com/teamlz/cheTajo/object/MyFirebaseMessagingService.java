de537781af133833368a6e4ee91673263ef45d11
giuggy <giuggy.rinaldi@gmail.com>
2016-06-03 11:29:11 +0200

    Push Notification: added 2 classes and  modified manifest and build.gradle

--- app/src/main/java/com/teamlz/cheTajo/object/MyFirebaseMessagingService.java
+++ app/src/main/java/com/teamlz/cheTajo/object/MyFirebaseMessagingService.java
@@ -1,28 +1,49 @@
 package com.teamlz.cheTajo.object;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
-import android.app.PendingIntent;
-import android.app.NotificationManager;
-import android.app.Notification;
-import android.content.Context;
 
-
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 import com.teamlz.cheTajo.R;
 import com.teamlz.cheTajo.activity.SplashActivity;
 
-public class Notify{
-    private int follower;
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    public Notify(int follower){
-        this.follower=follower;
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /*
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
     }
-    public void sendNotification(View view) {
+    // [END receive_message]
 
+    /*
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(View view, int follower) {
         // Use NotificationCompat.Builder to set up our notification.
         NotificationCompat.Builder builder = new NotificationCompat.Builder(view.getContext());
 
@@ -69,10 +90,5 @@ public class Notify{
 
         // Will display the notification in the notification bar
         notificationManager.notify(0, noty);
-
-
-
-
-
     }
 }
